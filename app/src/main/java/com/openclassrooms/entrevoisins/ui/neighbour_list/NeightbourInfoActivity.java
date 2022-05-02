de88fbda707775b0e622c83c18a1f7423f25f99b
Monokouma <aurange.thomas@gmail.com>
2022-05-02 20:48:59 +0200

    set neighbour info page

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeightbourInfoActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeightbourInfoActivity.java
@@ -12,11 +12,14 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.DummyNeighbourRepository;
 import com.openclassrooms.entrevoisins.service.NeighbourRepository;
+
+import java.util.Locale;
 
 public class NeightbourInfoActivity extends AppCompatActivity  {
 
@@ -34,6 +37,7 @@ public class NeightbourInfoActivity extends AppCompatActivity  {
         TextView neighbourPhoneNumber = findViewById(R.id.neighbourPhoneNumber);
         TextView neighbourFacebookLink = findViewById(R.id.neighbourFacebookLink);
         TextView neighbourAboutMe = findViewById(R.id.neighbourAboutMe);
+        FloatingActionButton addToFavoriteButton = findViewById(R.id.addToFavoriteButton);
 
         titleInToolbar.setTitle(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName());
         Glide.with(this).load(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getAvatarUrl()).placeholder(R.drawable.ic_account)
@@ -41,8 +45,9 @@ public class NeightbourInfoActivity extends AppCompatActivity  {
         titleInInfoStackView.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName());
         neighbourAdress.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getAddress());
         neighbourPhoneNumber.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getPhoneNumber());
-        neighbourFacebookLink.setText("www.facebook.fr/"+ neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName());
+        neighbourFacebookLink.setText("www.facebook.fr/"+ neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName().toLowerCase(Locale.ROOT));
         neighbourAboutMe.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getAboutMe());
+
     }
 
     public static Intent navigate(Context c, long id) {
