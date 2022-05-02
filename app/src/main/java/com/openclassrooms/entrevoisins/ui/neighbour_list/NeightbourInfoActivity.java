527613918bb106ba9e2394e6b3992e812d6ab62f
Monokouma <aurange.thomas@gmail.com>
2022-05-02 20:23:21 +0200

    set neighbour info page

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeightbourInfoActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeightbourInfoActivity.java
@@ -6,9 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.DummyNeighbourRepository;
 import com.openclassrooms.entrevoisins.service.NeighbourRepository;
 
@@ -20,10 +26,23 @@ public class NeightbourInfoActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neightbour_info);
-        Log.i("OUI", String.valueOf(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)));
         NeighbourRepository neighbourRepository = DI.getNeighbourApiService();
-        Log.i("OUI", neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).toString());
-        
+        ImageView profileImage = findViewById(R.id.toolbarImage);
+        CollapsingToolbarLayout titleInToolbar = findViewById(R.id.collapsingToolbar);
+        TextView titleInInfoStackView = findViewById(R.id.neighbourName);
+        TextView neighbourAdress = findViewById(R.id.neighbourAdress);
+        TextView neighbourPhoneNumber = findViewById(R.id.neighbourPhoneNumber);
+        TextView neighbourFacebookLink = findViewById(R.id.neighbourFacebookLink);
+        TextView neighbourAboutMe = findViewById(R.id.neighbourAboutMe);
+
+        titleInToolbar.setTitle(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName());
+        Glide.with(this).load(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getAvatarUrl()).placeholder(R.drawable.ic_account)
+                .into(profileImage);
+        titleInInfoStackView.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName());
+        neighbourAdress.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getAddress());
+        neighbourPhoneNumber.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getPhoneNumber());
+        neighbourFacebookLink.setText("www.facebook.fr/"+ neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getName());
+        neighbourAboutMe.setText(neighbourRepository.getNeighbourByID(getIntent().getLongExtra(ARGS_NEIGHBOURID, 0)).getAboutMe());
     }
 
     public static Intent navigate(Context c, long id) {
@@ -31,5 +50,7 @@ public class NeightbourInfoActivity extends AppCompatActivity  {
         intent.putExtra(ARGS_NEIGHBOURID, id);
         return intent;
     }
+
+
 
 }
